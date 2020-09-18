13494bd3589e1ecf21d457142a0db75e25b73b4b
Mickaël Moreau <mickael.moreau.pro@gmail.com>
2020-09-18 22:02:10 +0200

    Update public_api.ts
    To keep build ok, missing ./lib detection in my default env...

--- projects/ng-image-slider/src/public_api.ts
+++ projects/ng-image-slider/src/public_api.ts
@@ -2,6 +2,7 @@
  * Public API Surface of ng-image-slider
  */
 
-export * from './lib/ng-image-slider.service';
-export * from './lib/ng-image-slider.component';
-export * from './lib/ng-image-slider.module';
+export * from 'projects/ng-image-slider/src/lib/ng-image-slider.service';
+export * from 'projects/ng-image-slider/src/lib/ng-image-slider.component';
+export * from 'projects/ng-image-slider/src/lib/ng-image-slider.module';
+
